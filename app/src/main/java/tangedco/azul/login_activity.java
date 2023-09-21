37f70836facc3dc5a154c92f910f6e363cc28725
EdwardEDSJ <edufernandes1403@gmail.com>
2023-09-21 19:21:52 -0300

    Initial commit

---
+++ app/src/main/java/tangedco/azul/login_activity.java
@@ -0,0 +1,14 @@
+package tangedco.azul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
